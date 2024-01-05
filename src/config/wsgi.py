7e2f16ecaf969dfe7ddcfac04f970ab20793db48
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-05 04:46:25 +0200

    ReInit

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for website project.
+WSGI config for quizWebsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizWebsite.settings')
 
 application = get_wsgi_application()
