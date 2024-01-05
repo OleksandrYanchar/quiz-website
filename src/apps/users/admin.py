93c8851a1b3edc51cf500e7a5867a4e5cb043b39
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-05 06:39:31 +0200

    UPDATE: changed user admin panel

---
+++ src/apps/users/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import User
+from django.utils.safestring import mark_safe
+
+class CustomUserAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("username",)}
+        
+    list_display = ('username', 'slug', 'bio', 'display_image')
+    list_filter = ("username", )
+    search_fields = ("username",)
+    readonly_fields = ('display_image', )
+    
+    fieldsets = (
+        (None, {"fields": (("username", "slug"),)}),
+        (None, {"fields": ("bio", "picture", "display_image")}),
+    )
+    
+    def display_image(self, obj):
+        if obj.picture:
+            return mark_safe(f'<img src={obj.picture.url} width="100" height="110"')
+            print(obj.picture.url)
+        else:
+            return "No Image"
+
+
+    display_image.short_description = "Image"
+
+admin.site.register(User, CustomUserAdmin)
