4f4d056518588a2907957d88b31dd46addf9b00c
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-05 10:22:00 +0200

    UPDATE: added created and changed date fields

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -9,3 +9,5 @@ class User(AbstractUser):
     bio = models.TextField(blank=True, null=True    )
     picture = models.ImageField(upload_to='avatars/', blank=True, null=True)
     slug = models.SlugField(max_length=256)
+    date_added = models.DateTimeField(auto_now=True)
+    date_changed = models.DateTimeField(auto_now_add=True)
