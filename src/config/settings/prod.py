340156ccd7d4ae4776e22851b4171fa862343663
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-05 06:37:36 +0200

    UPDATED db settings

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -4,8 +4,18 @@ from dotenv import load_dotenv
 
 from .base import *
 
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = []  # Add hosts before deploy
 
 load_dotenv()
 
-DEBUG = bool(os.environ.get("DEBUG"))
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
