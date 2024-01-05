2cf2ef12a5bdf5c1e8a99eb15ef6a20ed81988a1
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-05 06:38:45 +0200

    Init: changed base user model

---
+++ src/apps/users/models.py
@@ -0,0 +1,11 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from core.models import BaseModel
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    bio = models.TextField(blank=True, null=True    )
+    picture = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    slug = models.SlugField(max_length=256)
