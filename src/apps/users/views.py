1bc84dc6ea985e572e02eb009a1adc1598f8f2e8
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-05 10:22:59 +0200

    UPDATE: added user and all user views

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from .serializers import UserSerializer, UserListSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class UserView(APIView):
+    def get(self, request, pk):
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+class UserListView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
