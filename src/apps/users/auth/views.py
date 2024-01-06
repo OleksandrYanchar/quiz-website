c82900ce9ba56447316bbe7edc531ed5c43a7c09
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-06 03:46:42 +0200

    Init:Login and Registration views

---
+++ src/apps/users/auth/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import authenticate
+from rest_framework import  status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserAuthSerializer
+from .tokens import create_jwt_pair_for_user
+
+
+class UserRegistrationView(APIView):
+    def post(self, request: Request):
+        serializer = UserAuthSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(request.data.get("password"))
+            user.save()
+            return Response(
+                data={
+                    "user_id": str(user.id),
+                    "tokens": create_jwt_pair_for_user(user=user),
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    def post(self, request: Request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return Response(
+                data={
+                    "user_id": str(user.id),
+                    "tokens": create_jwt_pair_for_user(user=user),
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(data={"message": "Invalid username or password"})
