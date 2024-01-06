f7f49ae16a46ab5e363eb5a3ef3263a86f357ec1
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-06 03:42:00 +0200

    UPDATE: added token endpoints

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 
 
 urlpatterns = [
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('all/', views.UserListView.as_view()),
     path('<str:pk>/',views.UserView.as_view()),
-    
+
 ]
