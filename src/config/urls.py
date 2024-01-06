78a95bedd3cbd4ce3c5216646ec3f54fc76cb0e4
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-06 03:42:44 +0200

    UPDATE: added auth urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,12 +4,19 @@ from django.urls import path, include
 
 from django.conf import settings
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('users.urls')),
-
+    path('', include('users.auth.urls')),
     
 ]
 
+"""path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), """
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
