fa9e15ade27233b1a4dcffc3e29b2fa7bca13f5d
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-06 03:40:02 +0200

    UPATE: changes is_active default to False

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -6,8 +6,9 @@ from core.models import BaseModel
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    bio = models.TextField(blank=True, null=True    )
+    bio = models.TextField(blank=True, null=True)
     picture = models.ImageField(upload_to='avatars/', blank=True, null=True)
     slug = models.SlugField(max_length=256)
     date_added = models.DateTimeField(auto_now=True)
     date_changed = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
