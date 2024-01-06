9a26d20640c9e25ac70a162fe4b1379b775570e3
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-06 06:23:51 +0200

    UPDATE: added email verifications views

--- src/apps/users/auth/views.py
+++ src/apps/users/auth/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth import authenticate
-from rest_framework import  status
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from users.models import User
 from .serializers import UserAuthSerializer
-from .tokens import create_jwt_pair_for_user
+from .tokens import EmailVerificationTokenGenerator, create_jwt_pair_for_user
 
 
 class UserRegistrationView(APIView):
@@ -15,6 +19,7 @@ class UserRegistrationView(APIView):
             user = serializer.save()
             user.set_password(request.data.get("password"))
             user.save()
+            self._send_email_verification(user)
             return Response(
                 data={
                     "user_id": str(user.id),
@@ -23,7 +28,33 @@ class UserRegistrationView(APIView):
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def _send_email_verification(self, user: User):
+        current_site = get_current_site(self.request)
+        subject = 'Activate Your Account'
+        body = render_to_string(
+            'email_verification.html',
+            {
+                "user": user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': EmailVerificationTokenGenerator().make_token(user),
+            }
+        )
+        user.email_user(subject=subject, message=body)
 
+
+class ActivateView(APIView):
+    def get(self, request, uidb64, token):
+        unique_id = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=unique_id)
+
+        if user and EmailVerificationTokenGenerator().check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response({"message": "Account activated", "status": status.HTTP_200_OK})
+        
+        return Response({"message": "Account activation failed", "status": status.HTTP_400_BAD_REQUEST})
 
 class UserLoginView(APIView):
     def post(self, request: Request):
