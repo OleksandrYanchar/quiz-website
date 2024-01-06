5d36c06b9608445f7411f9fa21eb40050a79b316
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-06 15:51:09 +0200

    UPDATE: added reset password serializer

--- src/apps/users/auth/serializers.py
+++ src/apps/users/auth/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from rest_framework.validators import ValidationError
-
+from django.contrib.auth import password_validation
 from users.models import User
 
 
@@ -13,3 +12,29 @@ class UserAuthSerializer(serializers.ModelSerializer):
             "email",
             "password",
         ]
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(max_length=128, write_only=True, required=True)
+    new_password1 = serializers.CharField(max_length=128, write_only=True, required=True)
+    new_password2 = serializers.CharField(max_length=128, write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError(
+                _('Your old password was entered incorrectly. Please enter it again.')
+            )
+        return value
+
+    def validate(self, data):
+        if data['new_password1'] != data['new_password2']:
+            raise serializers.ValidationError({'new_password2': _("The two password fields didn't match.")})
+        password_validation.validate_password(data['new_password1'], self.context['request'].user)
+        return data
+
+    def save(self, **kwargs):
+        password = self.validated_data['new_password1']
+        user = self.context['request'].user
+        user.set_password(password)
+        user.save()
+        return user
