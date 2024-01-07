2954aa427ace9f151f58901c1d11f6460e99299c
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-07 07:33:28 +0200

    UPDATE: added validations to all fields

--- src/apps/users/auth/serializers.py
+++ src/apps/users/auth/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import password_validation
 from users.models import User
-
+from django.contrib.auth.password_validation import validate_password 
+from django.core.exceptions import ValidationError
+from rest_framework.serializers import ValidationError
 
 class UserAuthSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,6 +15,18 @@ class UserAuthSerializer(serializers.ModelSerializer):
             "password",
         ]
 
+    def validate_email(self, value):
+        # Check if the email is unique
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
+
+    def validate_password(self, value):
+        try:
+            validate_password(password=value)
+        except ValidationError as e:
+            raise ValidationError(e.messages)
+        return value
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(max_length=128, write_only=True, required=True)
     new_password1 = serializers.CharField(max_length=128, write_only=True, required=True)
@@ -21,15 +35,18 @@ class ChangePasswordSerializer(serializers.Serializer):
     def validate_old_password(self, value):
         user = self.context['request'].user
         if not user.check_password(value):
-            raise serializers.ValidationError(
-                _('Your old password was entered incorrectly. Please enter it again.')
-            )
+            raise serializers.ValidationError('Your old password was entered incorrectly. Please enter it again.')
         return value
 
     def validate(self, data):
         if data['new_password1'] != data['new_password2']:
-            raise serializers.ValidationError({'new_password2': _("The two password fields didn't match.")})
-        password_validation.validate_password(data['new_password1'], self.context['request'].user)
+            raise serializers.ValidationError({'new_password2': 'The two password fields didn\'t match.'})
+        
+        try:
+            password_validation.validate_password(data['new_password1'], self.context['request'].user)
+        except ValidationError as e:
+            raise serializers.ValidationError({'new_password1': e.messages})
+        
         return data
 
     def save(self, **kwargs):
