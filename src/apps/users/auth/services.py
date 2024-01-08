7b50a65628057ca76cc11d6c765b023ca05d5d57
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-01-08 06:05:26 +0200

    UPDATE: made more abstract

--- src/apps/users/auth/services.py
+++ src/apps/users/auth/services.py
@@ -3,13 +3,10 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
-from .tokens import EmailVerificationTokenGenerator  # Import your token generator
-from django.contrib.auth.password_validation import validate_password 
-from django.core.exceptions import ValidationError
-from rest_framework.serializers import ValidationError
+from .tokens import EmailTokenGenerator 
 
 
-def _send_email_verification(request, user, template):
+def _send_email(request, user, template, subject):
     current_site = get_current_site(request)
     subject = 'Activate Your Account'
     body = render_to_string(
@@ -18,7 +15,7 @@ def _send_email_verification(request, user, template):
             "user": user,
             'domain': current_site.domain,
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': EmailVerificationTokenGenerator().make_token(user),
+            'token': EmailTokenGenerator().make_token(user),
         }
     )
     user.email_user(subject=subject, message=body)
